e68ef68b82b49b440f1f6a1f6c34fb0fca5e70c2
zbarrett <36522330+zbarrett@users.noreply.github.com>
2020-10-06 16:18:13 -0500

    Added GPU functionality

--- imdb-pytorch.py
+++ imdb-pytorch.py
@@ -9,7 +9,7 @@ import torch.nn.functional as F
 from torch.utils.data import TensorDataset, DataLoader
 import torch.optim as optim
 
-
+from sklearn.model_selection import train_test_split
 from keras.datasets import imdb
 from keras.preprocessing import sequence
 
@@ -22,28 +22,35 @@ batch_size = 32
 
 print('Loading data...')
 (X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=max_features)
-print(len(X_train), 'train sequences')
-print(len(X_test), 'test sequences')
 
-print('Pad sequences (samples x time)')
+# splitting test into test and val
+X_val, X_test, y_val, y_test = train_test_split(X_test, y_test, test_size=0.5)
+
+# padding to ensure appropriate dims
 X_train = sequence.pad_sequences(X_train, maxlen=maxlen)
+X_val = sequence.pad_sequences(X_val, maxlen=maxlen)
 X_test = sequence.pad_sequences(X_test, maxlen=maxlen)
-print('x_train shape:', X_train.shape)
-print('x_test shape:', X_test.shape)
+
+print('X_train shape:', X_train.shape)
+print('X_val shape:', X_val.shape)
+print('X_test shape:', X_test.shape)
 
 # converting np arrays to tensors
 train_data = TensorDataset(torch.from_numpy(X_train), torch.from_numpy(y_train))
+val_data = TensorDataset(torch.from_numpy(X_val), torch.from_numpy(y_val))
 test_data = TensorDataset(torch.from_numpy(X_test), torch.from_numpy(y_test))
 
 # creating data loader
-train_loader = DataLoader(train_data, shuffle=True, batch_size=batch_size)
-test_loader = DataLoader(test_data, shuffle=True, batch_size=batch_size)
+train_loader = DataLoader(train_data, shuffle=True, batch_size=batch_size, drop_last=True)
+val_loader = DataLoader(val_data, shuffle=True, batch_size=batch_size, drop_last=True)
+test_loader = DataLoader(test_data, shuffle=True, batch_size=batch_size, drop_last=True)
 
 
-# Begin modelling
+
+# MODELLING
+
 
 # defining model
-
 class Net(nn.Module):
     def __init__(self, vocab_size, embedding_dim, hidden_dim, n_layers=1):
         super(Net, self).__init__()
@@ -76,12 +83,24 @@ class Net(nn.Module):
 
     def init_hidden(self, batch_size):
         weight = next(self.parameters()).data
-        hidden = (weight.new(self.n_layers, batch_size, self.hidden_dim).zero_(),
-                  weight.new(self.n_layers, batch_size, self.hidden_dim).zero_())
+        hidden = (weight.new(self.n_layers, batch_size, self.hidden_dim).zero_().to(device),
+                  weight.new(self.n_layers, batch_size, self.hidden_dim).zero_().to(device))
         return hidden
 
 
+# determining device to use
+# torch.cuda.is_available() checks and returns a Boolean True if a GPU is available, else it'll return False
+is_cuda = torch.cuda.is_available()
+
+# If we have a GPU available, we'll set our device to GPU. We'll use this device variable later in our code.
+if is_cuda:
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+
+
 model = Net(vocab_size=max_features, embedding_dim=maxlen, hidden_dim=512)
+model.to(device)
 print(model)
 
 # creating loss & optimizer
@@ -89,11 +108,12 @@ criterion = nn.BCELoss()
 optimizer = torch.optim.Adam(model.parameters())
 
 
-epochs = 30
+epochs = 2
 counter = 0
 print_every = 100
 clip = 5
 valid_loss_min = np.Inf
+
 
 model.train()
 for i in range(epochs):
@@ -101,6 +121,7 @@ for i in range(epochs):
     for inputs, labels in train_loader:
         counter += 1
         h = tuple([e.data for e in h])
+        inputs, labels = inputs.to(device), labels.to(device)
         model.zero_grad()
         output, h = model(inputs, h)
         loss = criterion(output.squeeze(), labels.float())
@@ -109,26 +130,49 @@ for i in range(epochs):
         optimizer.step()
 
         if counter % print_every == 0:
-            # val_h = model.init_hidden(batch_size)
-            # val_losses = []
-            # model.eval()
-            # for inp, lab in val_loader:
-            #     val_h = tuple([each.data for each in val_h])
-            #     inp, lab = inp.to(device), lab.to(device)
-            #     out, val_h = model(inp, val_h)
-            #     val_loss = criterion(out.squeeze(), lab.float())
-            #     val_losses.append(val_loss.item())
+            val_h = model.init_hidden(batch_size)
+            val_losses = []
+            model.eval()
+            for inp, lab in val_loader:
+                val_h = tuple([each.data for each in val_h])
+                inp, lab = inp.to(device), lab.to(device)
+                out, val_h = model(inp, val_h)
+                val_loss = criterion(out.squeeze(), lab.float())
+                val_losses.append(val_loss.item())
 
             model.train()
-            print("Epoch: {}/{}...".format(i + 1, epochs),
+            print("Epoch: {}/{}...".format(i+1, epochs),
                   "Step: {}...".format(counter),
-                  "Loss: {:.6f}...".format(loss.item()))
+                  "Loss: {:.6f}...".format(loss.item()),
+                  "Val Loss: {:.6f}".format(np.mean(val_losses)))
             # if np.mean(val_losses) <= valid_loss_min:
             #     torch.save(model.state_dict(), './state_dict.pt')
             #     print('Validation loss decreased ({:.6f} --> {:.6f}).  Saving model ...'.format(valid_loss_min,
             #                                                                                     np.mean(val_losses)))
             #     valid_loss_min = np.mean(val_losses)
 
+
+
+
+test_losses = []
+num_correct = 0
+h = model.init_hidden(batch_size)
+
+model.eval()
+for inputs, labels in test_loader:
+    h = tuple([each.data for each in h])
+    inputs, labels = inputs.to(device), labels.to(device)
+    output, h = model(inputs, h)
+    test_loss = criterion(output.squeeze(), labels.float())
+    test_losses.append(test_loss.item())
+    pred = torch.round(output.squeeze())  # Rounds the output to 0/1
+    correct_tensor = pred.eq(labels.float().view_as(pred))
+    correct = np.squeeze(correct_tensor.cpu().numpy())
+    num_correct += np.sum(correct)
+
+print("Test loss: {:.3f}".format(np.mean(test_losses)))
+test_acc = num_correct/len(test_loader.dataset)
+print("Test accuracy: {:.3f}%".format(test_acc*100))
 
 
 
